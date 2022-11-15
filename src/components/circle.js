f357346c3eab9f9916ffe246739b8e135cde4a13
ti-abhishek <ti.abhishek15@gmail.com>
2022-11-16 02:27:10 +0530

    new

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
+var Shape = require('./shape.js');
 
 // declare class
+class Circle extends Shape {
+    constructor(color) {
+         super();
+    }
+    calculateArea() {
 
+    }
+}
 
 // export class using module.exports
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
+class Shape {
+    constructor() {
+         this.color = "green";
+    }
+    drawShape() {
+
+    }
+    calculateArea() {
+
+    }
+}
 
 // export class using module.exports
+module.exports = Shape;
